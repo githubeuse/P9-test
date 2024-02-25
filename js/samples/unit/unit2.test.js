41a982fa8d06a96570e931e677b078e1d6aab037
githubeuse <julie.souvandy.pro@gmail.com>
2024-02-25 15:21:42 +0100

    test 2

--- js/samples/unit/unit2.test.js
+++ js/samples/unit/unit2.test.js
@@ -1,7 +1,7 @@
 import { getAge } from './unit2.js'
 
 describe('getAge Unit Test Suites', () => {
-    it('should render "Vous avez actuellement 31 ans"', () => {
-        expect(getAge(31)).toBe("Vous avez actuellement 31 ans")
+    it('should render "Vous avez actuellement 32 ans"', () => {
+        expect(getAge(32)).toBe("Vous avez actuellement 32 ans")
     })
 })
